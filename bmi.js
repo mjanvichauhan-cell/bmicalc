9225eddf2d3ad5afce2e6e23d556f732dc3e9410
mjanvichauhan-cell <mjanvichauhan@gmail.com>
2026-09-15 18:24:57 +0530

    Update

--- bmi.js
+++ bmi.js
@@ -1,6 +1,4 @@
 const form = document.querySelector('form');
-// this usecase will give you empty
-// const height = parseInt(document.querySelector('#height').value)
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -15,7 +13,6 @@ form.addEventListener('submit', function (e) {
     results.innerHTML = `Please give a valid weight ${weight}`;
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    //show the result
     results.innerHTML = `<span>${bmi}</span>`;
   }
 });
